f6df64f49156b36679881666e6e4dfd490102116
Vinicius Monteiro <vmonteirosamuel91@outlook.com>
2020-08-29 01:53:15 +0100

    Scene transition

---
+++ Assets/Script/RPGCoreScripts/SceneManagement/Fader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    CanvasGroup cg;
+    [SerializeField]bool fadeIn;
+    [SerializeField]bool fadeOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cg = GetComponent<CanvasGroup>();
+    }
+
+    void FadeIn()
+    {
+        cg.alpha += 0.3f * Time.deltaTime;
+    }
+
+    void FadeOut()
+    {
+        cg.alpha -= 0.3f * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeIn)
+            FadeIn();
+        if (fadeOut)
+            FadeOut();
+    }
+}
---
+++ Assets/Script/RPGCoreScripts/SceneManagement/Portal.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace RPG.SceneManagement
+{
+    using UnityEngine;
+    using UnityEngine.AI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine.SceneManagement;
+    using System;
+
+    public enum Destination
+    {
+        A, B, C, D, E
+    }
+
+    public class Portal : MonoBehaviour
+    {
+        public Transform spawnPoint;
+        public Destination destination;
+        
+        [SerializeField] int sceneToLoad = 0;
+
+        GameObject player;
+        // Start is called before the first frame update
+        void Start()
+        {
+            spawnPoint = transform.GetChild(0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                StartCoroutine(SceneTransition());
+            }
+        }
+
+        public IEnumerator SceneTransition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
+            print("Scene Loaded");
+            Destroy(gameObject);
+
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().enabled = false;
+            player.transform.position = otherPortal.spawnPoint.transform.position;
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+            player.GetComponent<NavMeshAgent>().enabled = true;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+                return portal;
+
+            }
+
+            return null;
+        }
+    }
+
+}
